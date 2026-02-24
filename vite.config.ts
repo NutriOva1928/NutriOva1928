1b895d687f9480353f4baa17f0983739a69c1556
NutriOva1928 <sofiamirandagarcia018@gmail.com>
2026-02-24 14:12:01 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://rogeraviles22.github.io/NutriHealth',
+  base: 'https://nutriova1928.github.io/NutriOva1928',
 })
